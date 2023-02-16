265aeaf5fc1995bace5a14b2363cca90978234f3
Adelina Gainullina <71651929+fltkbyf6adelina696@users.noreply.github.com>
2023-02-16 09:47:45 +0200

    Add files via upload

---
+++ Kopilka, lab1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class HelloWorld
+{
+public static void Main(string[] args)
+{
+
+//Ввод какого-либо предмета, на который пользователь хочет накопить
+Console.WriteLine("Вы копите на ");
+
+//Название этого предмета
+string name = Console.ReadLine();
+
+//Программа спрашивает, сколько стоит эта вещь
+Console.WriteLine("Сколько стоит " + name.ToString() + "?");
+
+//Сколько стоит эта вещь
+string allsumString = Console.ReadLine();
+
+//Преобразование строки в число
+int allsumArg = Convert.ToInt32(allsumString);
+
+//Первоначальная сумма всегда будет равняться нулю
+int begin = 0;
+
+while (begin < allsumArg) {
+
+
+Console.WriteLine("Сколько хотите сейчас внести?");
+
+//Внесение денег
+string sumString = Console.ReadLine();
+
+//Преобразование строки в число
+int sumArg = Convert.ToInt32(sumString);
+
+begin = begin + sumArg;
+
+if((allsumArg-begin) <= 0) {
+Console.WriteLine();
+} else {
+Console.WriteLine("Осталось накопить " + (allsumArg-begin));
+}
+
+}
+
+//Вывод результата
+Console.WriteLine("Поздравляю! Вы накопили на " + name.ToString());
+
+}
+}
